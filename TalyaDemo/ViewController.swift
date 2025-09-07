84c2be239e2052f21155dc942950246e36d6260c
liusilan <liusl6@chinatelecom.cn>
2025-09-07 12:00:09 +0800

    Initial Commit

---
+++ TalyaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalyaDemo
+//
+//  Created by liusilan on 2025/9/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TalyaDemoTests/TalyaDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TalyaDemoTests.swift
+//  TalyaDemoTests
+//
+//  Created by liusilan on 2025/9/7.
+//
+
+import Testing
+@testable import TalyaDemo
+
+struct TalyaDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
